339897610c49c97f3b08e2394e0138ad0877d052
HugoD66 <dessauw.hugo@gmail.com>
2024-04-15 09:11:49 +0200

    init map

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,16 +1,31 @@
-import {useEffect, useRef, useState} from "react";
-import maplibregl from 'maplibre-gl';
-const Map = () => {
+import { useEffect, useRef, useState } from "react";
+import maplibregl, { Marker } from 'maplibre-gl';
 
-  const mapContainer = useRef(null);
-  const map = useRef(null);
+const Map = () => {
+  const mapContainer = useRef<HTMLDivElement>(null);
+  const map = useRef<maplibregl.Map | null>(null);
+  const currentMarker = useRef<Marker | null>(null);
   const [lng] = useState(139.753);
   const [lat] = useState(35.6844);
   const [zoom] = useState(14);
-  const [API_KEY] = useState('YOUR_MAPTILER_API_KEY_HERE');
+  const [API_KEY] = useState('1bYmKrc8pg0FSu8GXalV');
 
-  useEffect(() => {
-    if (map.current) return; // stops map from intializing more than once
+
+  return (
+    <div>
+      <div className="map-wrap">
+        <div ref={mapContainer} className="map" />
+      </div>
+    </div>
+  );
+};
+
+export default Map;
+/*
+      <button onClick={addMarker}>Ajouter un point</button>
+
+ useEffect(() => {
+    if (map.current || !mapContainer.current) return;
 
     map.current = new maplibregl.Map({
       container: mapContainer.current,
@@ -18,12 +33,19 @@ const Map = () => {
       center: [lng, lat],
       zoom: zoom
     });
-
   }, [API_KEY, lng, lat, zoom]);
-  return (
-    <div className="map-wrap">
-      <div ref={mapContainer} className="map"/>
-    </div>
-  );
-}
-export default Map;
+
+  const addMarker = () => {
+
+    if (!map.current) return;
+    /*
+     // Supprime le marqueur précédent s'il existe
+     if (currentMarker.current) {
+       currentMarker.current.remove();
+     }
+currentMarker.current = new Marker({ color: `#FF0000` })
+  .setLngLat([lng, lat])
+  .addTo(map.current);
+};
+
+ */
