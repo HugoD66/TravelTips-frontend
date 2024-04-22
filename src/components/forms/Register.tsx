63ea923b8137881a8da7bf1a17edd61cc08c6a7d
melinaaaa0805 <melina.mitterrand08@gmail.com>
2024-04-22 09:38:01 +0200

    pb date naissance

--- src/components/forms/Register.tsx
+++ src/components/forms/Register.tsx
@@ -38,17 +38,17 @@ const Register = ({ goChangeForm }: { goChangeForm: () => void }) => {
       return;
     }
 
-    // Validation de la date de naissance
-    const birthdateRegex =
-      /^(0[1-9]|[12][0-9]|3[01])\/(0[1-9]|1[012])\/(19[2-9]\d|200[0-9]|201[0-5])$/;
-    if (!birthdateRegex.test(birthday)) {
-      setError(
-        new Error(
-          "La date de naissance doit être au format jj/mm/aaaa et comprise entre 1920 et 2015"
-        )
-      );
-      return;
-    }
+    // // Validation de la date de naissance
+    // const birthdateRegex =
+    //   /^(0[1-9]|[12][0-9]|3[01])\/(0[1-9]|1[012])\/(19[2-9]\d|200[0-9]|201[0-5])$/;
+    // if (!birthdateRegex.test(birthday)) {
+    //   setError(
+    //     new Error(
+    //       "La date de naissance doit être au format jj/mm/aaaa et comprise entre 1920 et 2015"
+    //     )
+    //   );
+    //   return;
+    // }
 
     // Validation de l'adresse e-mail
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
